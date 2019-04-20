e04416879d0975626e1ca25cd680eba9eb50b9a9
Marc-Andre <marcadenis@msn.com>
2019-04-20 14:32:43 -0400

    Ported the template for the motherboard

---
+++ APP/APP_heartBeat.c
@@ -0,0 +1,23 @@
+/*
+ * heartBeat.h
+ *
+ *  Created on: 14 mars 2019
+ *      Author: Marc-Andre
+ *
+ *      This thread blinks a led to show that the board is alive
+ *
+ */
+
+
+#include "APP_heartBeat.h"
+#include "stm32f4xx_hal.h"
+
+void tsk_heartBeat(void const * argument){
+
+	while(1){
+		HAL_GPIO_WritePin(HEARTBEAT_LED_PORT,HEARTBEAT_LED_PIN,1);
+		osDelay(50);
+		HAL_GPIO_WritePin(HEARTBEAT_LED_PORT,HEARTBEAT_LED_PIN,0);
+		osDelay(1000);
+	}
+}
